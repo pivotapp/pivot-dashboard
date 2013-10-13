2fb1b1d0b7b0fc28f61593168ad21f7cb8989531
Cameron Bytheway <cameron@theflokk.com>
2013-10-13 00:36:59 -0600

    first pass at sso token

--- app.js
+++ app.js
@@ -4,11 +4,34 @@
 
 var stack = require('simple-stack-ui');
 var envs = require('envs');
+var format = require('url').format;
 
-var app = module.exports = stack();
+var app = module.exports = stack({
+  restricted: true
+});
+
+app.useAfter('base', '/apps', function accessToken(req, res, next) {
+  if (!req.query._access_token) return next();
+
+  var basePath = (req.get('x-orig-path') || '') + req._parsedUrl.pathname;
+
+  res.cookie('_access_token', req.query._access_token, {
+    secure: ~req.base.indexOf('https://'),
+    path: basePath
+  });
+
+  delete req.query._access_token;
+
+  var href = format({
+    pathname: basePath,
+    query: req.query
+  });
+
+  res.redirect(href);
+});
 
 app.locals({
   env: {
     API_URL: '/api'
   }
-})
+});
