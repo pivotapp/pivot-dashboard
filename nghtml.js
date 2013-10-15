640858d21be90d339194be96d411253fcff45284
Cameron Bytheway <cameron@theflokk.com>
2013-10-15 16:39:01 -0600

    fix duplicate template problem

--- nghtml.js
+++ nghtml.js
@@ -9,6 +9,7 @@ module.exports = nghtml({
   webroot: 'public',
   'module': 'pivot',
   extension: '.jade',
+  confProp: 'angular-templates',
   hook: function (content) {
     var compile = jade.compile(content);
     return compile();
