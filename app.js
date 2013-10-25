64127e75f4429bf2cd3e4bc3808caffee810ea1b
Cameron Bytheway <cameron@theflokk.com>
2013-10-25 17:01:38 -0600

    update simple-stack-ui

--- app.js
+++ app.js
@@ -4,15 +4,13 @@
 
 var stack = require('simple-stack-ui');
 var sso = require('./lib/sso-access-token');
-var routes = require('./public/javascripts/routes');
 
 /**
  * Expose the app
  */
 
 var app = module.exports = stack({
-  restricted: false,
-  routes: routes
+  restricted: false
 });
 
 /**
@@ -25,9 +23,4 @@ app.useAfter('base', '/apps', sso());
  * Setup app-wide locals
  */
 
-app.locals({
-  app: 'pivot-dashboard',
-  env: {
-    API_URL: '/api'
-  }
-});
+app.env('API_URL', '/api');
